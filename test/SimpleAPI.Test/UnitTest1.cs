3b6dfce536536109eb905d1addcc31123faa1d94
lhc\masuniw <wesley.masunika@lifehealthcare.co.za>
2020-07-30 10:45:58 +0200

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using SimpleAPI;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController controller = new WeatherForecastController();
+        WeatherForecast weatherForecast ;
+
+    	[Fact]
+        public void GetWeatherResults()
+        {
+            var returnValue = controller.Get();
+
+
+            Assert.NotNull(returnValue);
+
+            //Assert.IsType(weatherForecast, returnValue.GetEnumerator());
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
